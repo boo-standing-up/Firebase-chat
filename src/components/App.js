c7bf7bfb264b636e3b55d9ff46a8ac919cf1437f
boo <standingup.boo@gmail.com>
2022-02-05 21:47:42 +0900

    ✅ メインで作業したいから、config入れたぞ

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import SignIn from './Singin';
 import Main from './main';
+import config from '../config.json';
+
 
 export default () => {
   const [name,setName]=useState('');
   console.log({name});
-  if (name===""){
+  if (config.SignInEnabled && name===""){
     return<SignIn setName={setName}/>;
   }
   
